045aa2ed9466879a870dbf2d257bf835ae5e7e53
sarfaraz187 <sarfarazmohammed187@gmail.com>
2024-05-14 18:39:13 +0200

    typescript and nodemon installed.

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
-import express from "express";
+import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
 
 dotenv.config();
 
-const app = express();
-const port = process.env.PORT;
+const app: Express = express();
+const port = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
+app.get("/", (req: Request, res: Response) => {
   res.send("Express + TypeScript Server");
 });
 
