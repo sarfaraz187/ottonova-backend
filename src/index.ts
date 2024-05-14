7154c56958752bae3e6eafe93d5e454450e854f1
sarfaraz187 <sarfarazmohammed187@gmail.com>
2024-05-14 19:17:55 +0200

    cities route and JSON response payload added

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,20 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
+import { City } from "../models/city.ts";
+import citiesJson from "../data/cities.json";
 
 dotenv.config();
-
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("Express + TypeScript Server");
+  res.send("Express + TypeScript Server jjj");
+});
+
+app.get("/cities", (req: Request, res: Response) => {
+  const cities: City[] = citiesJson.cities;
+  console.log("Available Cities :", cities);
+  res.send(cities);
 });
 
 app.listen(port, () => {
